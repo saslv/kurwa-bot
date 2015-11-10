42742f45ee7e51e9762957c7ab282eefd36d6c9e
Yaroslav Hasko <yar.hasko@gmail.com>
2015-11-10 13:34:03 +0200

    fixed version output

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var db = new sqlite3.Database('bot.sqlite');
 
 var version = {
     number: shell.exec('git log --pretty=oneline | wc -l', {silent: true}).output.replace(/(\n|\r)+$/, ''),
-    message: shell.exec('git log -1 --pretty=%B', {silent: true}).output.replace(/(\n|\r)+$/, '')
+    message: shell.exec('git log -1 --oneline', {silent: true}).output.replace(/(\n|\r)+$/, '')
 }
 
 /*
@@ -52,6 +52,14 @@ bot.onText(/300/, function (msg) {
 
 bot.onText(/триста/, function (msg) {
     bot.sendMessage(msg.chat.id, '@' + msg.from.username + ', відсоси у тракториста!');
+});
+
+bot.onText(/amputee/, function (msg) {
+    bot.sendMessage(msg.chat.id, '@' + msg.from.username + ', хворий виблядок!');
+});
+
+bot.onText(/porn/, function (msg) {
+    bot.sendMessage(msg.chat.id, '@' + msg.from.username + ', хворий виблядок!');
 });
 
 bot.onText(/\/swear/, function (msg) {
